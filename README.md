46c159d2e2df33248002f4a1555ab93b8399e37e
Ouraborus <1222eca@gmail.com>
2017-11-27 00:41:14 -0500

    Fibonacci layout updated, added blog page

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # hb-first-page
 
 [![Travis](https://img.shields.io/travis/rust-lang/rust.svg?style=for-the-badge)](https://github.com/Ouraborus/hb-first-page)
-[![Codecov](https://img.shields.io/codecov/c/github/codecov/example-python.svg?style=for-the-badge)](https://github.com/Ouraborus/hb-first-page)
 
 I'm using SASS as a CSS preprocessor and stylelint for SASS linting.
 
-Run in a development environment watching files with 'npm start'.
-Lint the .scss files with 'npm run lint'.
-Build an optimized CSS with 'npm run build'.
+Run in a development environment watching files with `npm start`.
+Lint the .scss files with `npm run lint`.
+Build an optimized CSS with `npm run build`.
---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+window.onload = () =>{ 
+    startEvents();
+};
+
+function startEvents() {
+    const plusIcon = document.getElementsByClassName('icon-plus')[0];
+    ['mouseover', 'mouseleave'].forEach(evt => plusIcon.addEventListener(evt, reveal));
+    plusIcon.addEventListener('click',fadeOut);
+}
+
+function reveal() { 
+    const img = document.getElementsByClassName('img-grid');
+    img[0].classList.toggle('hidden');
+    img[1].classList.toggle('hidden');
+}
+
+function fadeOut() {
+    const sections = Array.from(document.getElementsByClassName('container')[0].children);
+    let i = 0;
+    sections.forEach(element => {
+        progressiveFade(element,i);
+        i++;
+    });
+    console.log(sections);
+}
+
+function progressiveFade(element, i) {
+    element.classList.add('fade-'+i);
+}
